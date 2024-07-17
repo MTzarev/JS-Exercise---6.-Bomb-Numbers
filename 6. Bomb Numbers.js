905b60b8d34e30b6bc5b08547cc63816ac33a3cc
Miroslav Tsarev <mtzarev@gmail.com>
2024-07-17 23:39:44 +0300

    Update 6. Bomb Numbers.js

--- 6. Bomb Numbers.js
+++ 6. Bomb Numbers.js
@@ -16,7 +16,5 @@ for (let i=0; i<nums.length; i++){
     console.log(nums.reduce((a,b)=>a+b));
 }
 bombNumbers([1, 2, 2, 4, 2, 2, 2, 9], [4, 2]);
-bombNumbers([1, 4, 4, 2, 8, 9, 1], [9, 3]);
-bombNumbers([1, 7, 7, 1, 2, 3], [7, 1]);
-bombNumbers([1, 1, 2, 1, 1, 1, 2, 1, 1, 1], [2, 1]);
+
 
